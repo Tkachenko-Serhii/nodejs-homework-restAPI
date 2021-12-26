42513d0a9f493e3601885da9b1a367a80c7cb6c3
Serhii Tkachenko <79119130+Tkachenko-Serhii@users.noreply.github.com>
2021-12-26 23:30:57 +0200

    completed hw-03

---
+++ models/contact.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+
+const contactSchema = Schema({
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const joiSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  phone: Joi.string().min(3).max(15).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net', 'ua', 'ru'] },
+    })
+    .required(),
+  favorite: Joi.bool,
+});
+
+const Contact = model('contact', contactSchema);
+
+module.exports = { Contact, joiSchema };
